65b343557691f985391e39c2d526b8f786fbf0ff
Drikus <standerhc@live.com>
2018-11-06 17:50:21 +0200

    added search to table view and pulling model data from web need to build model

--- swifty-protein/swifty-protein/ViewController.swift
+++ swifty-protein/swifty-protein/ViewController.swift
@@ -8,22 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     var ligands : [Substring]?
+    var cuurentLigands : [Substring]?
+    var vectors : [[Substring]] = []
+    var connections : [[Substring]] = []
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (ligands?.count)!
+        return (cuurentLigands?.count)!
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ligandCell", for: indexPath) as! LigandTableViewCell
-        cell.ligand = String(ligands![indexPath.row])
+        cell.ligand = String(cuurentLigands![indexPath.row])
         return cell
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
         let file = "ligands"
         if let dir = Bundle.main.path(forResource: file, ofType: "txt"){
             let fm = FileManager()
@@ -32,6 +39,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                 let content = fm.contents(atPath: dir)
                 let contentAsString = String(data: content!, encoding: String.Encoding.utf8)
                 ligands = contentAsString?.split(separator: "\n")
+                cuurentLigands = ligands
             }
         }
 
@@ -39,5 +47,57 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
 
 
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        guard !searchText.isEmpty else {
+            cuurentLigands = ligands
+            self.tableView.reloadData()
+            return
+        }
+        cuurentLigands = ligands?.filter({ (Substring) -> Bool in
+            Substring.lowercased().contains(searchText.lowercased())
+        })
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedLigand = cuurentLigands![indexPath.row]
+        let url = URL(string: "https://files.rcsb.org/ligands/view/" + selectedLigand + "_model.sdf")
+        var request = URLRequest(url: url!)
+        request.httpMethod = "Get"
+        let task = URLSession.shared.dataTask(with: request){
+            (data, response, error) in
+            if error != nil{
+                print(error!)
+            }
+            else if let d = data {
+                let responseString = String(data: d, encoding: String.Encoding.utf8)!
+                let responseSplit = responseString.split(separator: "\n")
+                let countSplit = responseSplit[2].split(separator: " ")
+                let vectorsCount = Int(countSplit[0])
+                let connectionCount = Int(countSplit[1])
+                var i : Int = 0;
+                while i < vectorsCount!{
+                    self.vectors.append(responseSplit[i + 3].split(separator: " "))
+                    i += 1
+                }
+                i = 0
+                while i < connectionCount!{
+                    self.connections.append(responseSplit[i + vectorsCount! + 3].split(separator: " "))
+                    i += 1
+                }
+//                for split in self.vectors{
+//                    print(split)
+//                }
+//                for split in self.connections{
+//                    print(split)
+//                }
+                DispatchQueue.main.async {
+                    self.performSegue(withIdentifier: "modelSegue", sender: self)
+                }
+                
+            }
+        }
+        task.resume()
+    }
 }
 
